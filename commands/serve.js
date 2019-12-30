02f11f777a9eebe90b61b98c92d9aba9fc6a1dfc
William Gibson <w.gibbo3@icloud.com>
2019-12-30 05:43:54 +0000

    Add support for Mesa connections during surving

--- commands/serve.js
+++ commands/serve.js
@@ -2,8 +2,10 @@ const fs = require('fs')
 const ora = require('ora')
 const chalk = require('chalk')
 const inquirer = require('inquirer')
+const { Client } = require('@cryb/mesa')
 
 const { logSplitter } = require('../utils')
+const { url, token } = require('../config.json')
 const { createClient } = require('../drivers/docker.driver')
 
 /**
@@ -25,14 +27,62 @@ module.exports = async program => {
 		}
 	}
 
-	logSplitter('Serving Xen', {
-		newline: program.rawArgs.indexOf('serve') === -1
-	})
-
 	if(!fs.existsSync('bin/portal'))
 		require('./update')(program)
 
-	const client = createClient()
+	logSplitter('Connecting to Atlas', {
+		newline: program.rawArgs.indexOf('serve') === -1
+	})
+
+	let connectingToAtlas,
+		authenticatingWithAtlas,
+		reconnectingToAtlas
+
+	connectingToAtlas = ora(`Connecting to Atlas...`).start()
+
+	const client = new Client(url)
+
+	client.on('connected', async () => {
+		if(reconnectingToAtlas) {
+			reconnectingToAtlas.succeed('Reconnected to Atlas!')
+			reconnectingToAtlas = null
+		} else 
+			connectingToAtlas.succeed('Connected to Atlas!')
+
+		authenticatingWithAtlas = ora(`Authenticating with Atlas...`).start()
+		await client.authenticate({ token })
+		authenticatingWithAtlas.succeed('Authenticated with Atlas!')
+	})
+
+	client.on('message', message => {
+		ora(`Recieved ${message.serialize()} from Atlas`).info()
+	})
+
+	client.on('disconnected', (code, reason) => {
+		if(reconnectingToAtlas)
+			reconnectingToAtlas.stop()
+
+		if(authenticatingWithAtlas)
+			authenticatingWithAtlas.stop()
+		
+		reconnectingToAtlas = ora('Disconnected from Atlas - reconnecting...').start()
+	})
+
+	client.on('error', error => {
+		if(!reconnectingToAtlas) return
+
+		if(authenticatingWithAtlas)
+			authenticatingWithAtlas.stop()
+
+		reconnectingToAtlas.fail(`Atlas connection error: ${error.message}`)
+		reconnectingToAtlas = ora('Disconnected from Atlas - reconnecting...').start()
+	})
+
+	// logSplitter('Serving Xen', {
+	// 	newline: program.rawArgs.indexOf('serve') === -1
+	// })
+
+	// const client = createClient(url, token)
 	// 		building = ora('Building Docker image...').start()
 
 	// const stream = await client.buildImage({
@@ -60,17 +110,17 @@ module.exports = async program => {
 
 	// building.succeed('Docker image successfully built')
 
-	const starting = ora('Starting Docker image...').start(),
-			name = `xen-${Date.now()}`
-			container = await client.createContainer({
-				name,
-				hostname: name,
-				image: process.env.DOCKER_IMAGE || 'cryb/portal',
-				autoRemove: true,
-				networkMode: 'bridge',
-				shmSize: parseInt(process.env.DOCKER_SHM_SIZE || '1024') * 1048576
-			})
+	// const starting = ora('Starting Docker image...').start(),
+	// 		name = `xen-${Date.now()}`
+	// 		container = await client.createContainer({
+	// 			name,
+	// 			hostname: name,
+	// 			image: process.env.DOCKER_IMAGE || 'cryb/portal',
+	// 			autoRemove: true,
+	// 			networkMode: 'bridge',
+	// 			shmSize: parseInt(process.env.DOCKER_SHM_SIZE || '1024') * 1048576
+	// 		})
 
-	await container.start()
-	starting.succeed('Docker image started!')
+	// await container.start()
+	// starting.succeed('Docker image started!')
 }
